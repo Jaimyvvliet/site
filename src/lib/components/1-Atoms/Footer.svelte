c626dd708c8a6f295cb33486426a0af44d880aae
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2026-01-04 14:01:56 +0100

    add footer to +layout.svelte

---
+++ src/lib/components/1-Atoms/Footer.svelte
@@ -0,0 +1,12 @@
+  <footer>
+    <nav>
+      <ul>
+        <li><a href="biografie.html">Biografie</a></li>
+        <li><a href="blog.html">Blog</a></li>
+        <li><a href="contact.html">Contact</a></li>
+        <li><a href="https://open.spotify.com/artist/54MFcY18lL2m0CMJZsr7tb"  target="_blank" rel="noopener noreferrer">Spotify</a></li>
+        <li><a href="https://www.instagram.com/jaimyrafael/"  target="_blank" rel="noopener noreferrer">Instagram</a></li>
+      </ul>
+   <nav/>
+    <p>&copy; 2025 Jaimy Rafael</p>
+  </footer>
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,2 +1,3 @@
 // Reexport your entry components here
 export { default as Header } from './components/1-Atoms/Header.svelte';
+export { default as Footer } from './components/1-Atoms/Footer.svelte';
