65d4250f2a733047a03382d47499718a03eb602c
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2026-01-05 08:36:02 +0100

    added spotify component

--- src/lib/components/1-Atoms/Spotify.svelte
+++ src/lib/components/1-Atoms/Spotify.svelte
@@ -1,16 +1,27 @@
-<iframe 
-title="spotify"
-data-testid="embed-iframe" 
-style="border-radius:12px" 
-src="https://open.spotify.com/embed/artist/54MFcY18lL2m0CMJZsr7tb?utm_source=generator&theme=0" 
-width="100%" height="352" 
-frameBorder="0" allowfullscreen="" 
-allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
-loading="lazy">
-</iframe>
+<section>
+  <h2>Follow me op spotify!</h2>
+  <iframe 
+    title="spotify"
+    data-testid="embed-iframe"
+    src="https://open.spotify.com/embed/artist/54MFcY18lL2m0CMJZsr7tb?utm_source=generator"
+    width="100%" 
+    height="352" 
+    frameBorder="0" 
+    allowfullscreen="" 
+    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+    loading="lazy">
+  </iframe>
+</section>
+
 
 <style>
-  iframe {
 
+  section {
+    padding:var(--padding-container);
   }
+
+  section iframe {
+    border-radius: var(--border-radius);
+  }
+
 </style>
