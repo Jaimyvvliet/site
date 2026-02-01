878c5f99557b5f1e3c4131fb572d99516b56c68a
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2026-02-01 16:20:18 +0100

    update with logo footer

--- src/lib/components/4-Template/Footer.svelte
+++ src/lib/components/4-Template/Footer.svelte
@@ -1,10 +1,15 @@
 <script>
-	import { Wrapper, LinkSection, Copyright } from '$lib/index.js';
+	import { Wrapper, Icons, LinkSection, Copyright } from '$lib/index.js';
 	export let footerData;
 </script>
 
 <footer>
 	<Wrapper backgroundColor="var(--color-dark)">
+		<div>
+			<a href="/">
+				<Icons icon="Logo" backgroundColor="var(--color-primary)" color="var(--color-dark)"/>
+			</a>
+		</div>
 		<LinkSection sectionItems={footerData.footerLinks} />
 	</Wrapper>
 	<Copyright />
@@ -14,5 +19,25 @@
 	footer {
 		background: var(--color-dark);
 		color: var(--color-light);
+		position:relative;
+		display:grid;
+	}
+
+	footer div {
+		--ratio: 10rem;
+		width:100%;
+		height: calc(var(--ratio) / 2);
+		position:relative;
+	}
+	footer div a {
+		width:var(--ratio);
+		height:var(--ratio);
+		margin-top: -5rem;
+		position:absolute;
+		right:0;
+	}
+
+	footer div a:hover {
+		background:none;
 	}
 </style>
